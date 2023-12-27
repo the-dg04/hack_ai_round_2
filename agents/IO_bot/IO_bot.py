2876cd96159739c702d41a98eb97add137053619
the-dg04 <devgup04@gmail.com>
2023-12-27 16:39:16 +0530

    fdlfrk;efdrskfkfds

--- agents/IO_bot/IO_bot.py
+++ agents/IO_bot/IO_bot.py
@@ -14,7 +14,7 @@ class PercentMatchModel(Model):
 IO_bot=Agent(name="IO_bot",seed="IO_bot",port=8070,endpoint=["http://127.0.0.1:8070/submit"])
 fund_agent_if_low(IO_bot.wallet.address())
 
-@IO_bot.on_event("s;'loi;tartup")
+@IO_bot.on_event("startup")
 async def get_input(ctx:Context):
     # get file from ui
     resume_file="Cape-Town-Resume-Template-Retro-Creative.pdf"
